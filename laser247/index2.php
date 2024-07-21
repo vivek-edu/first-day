9561d6021176c8b788c6956f0716dc58830c4030
vivek-edu <iamprimeoptimus@gmail.com>
2024-07-21 23:55:59 +0530

    first

---
+++ laser247/index2.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+
+<body>
+    <nav class="navbar">
+        <img src="images/logo.gif" alt="logo">
+        <div>
+            <form action="http://localhost/laser247/form.php" method="post">
+                <span><i class="fa-solid fa-user"></i></span>
+                <input type="text" placeholder="Username" required="" name="username">
+                <input type="password" placeholder="Password" required="" name="password">
+                <button>Login <i class="fa-solid fa-arrow-right"></i></button>
+                <a href="register.php">Sign up <i class="fa-solid fa-arrow-right"></i></a>
+            </form>
+        </div>
+    </nav>
+
+    
+    <ul class="list">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">In-Play</a></li>
+        <li><a href="#">Cricket</a></li>
+        <li><a href="#">Football</a></li>
+        <li><a href="">Tennis</a></li>
+        <li><a href="#">Casino</a></li>
+        <li><a href="#">Int Casino</a></li>
+        <li><a href="#">Horse Racing</a></li>
+        <li><a href="#">Greyhound Racing</a></li>
+        <li><a href="#">Kabaddi</a></li>
+        <li><a href="#">Politics</a></li>
+        <li><a href="#">Sports Book</a></li>
+        <li><a href="#">Binary</a></li>
+        <li><a href="#">Mixed Material Arts</a></li>
+        <li><a href="#">Volleyball</a></li>
+        <li><a href="#">Ice Hockey</a></li>
+        <li><a href="#">Darts</a></li>
+        <li><a href="#">Basketball</a></li>
+        <li><a href="#">Baseball</a></li>
+        <li><a href="#">Futsal</a></li>
+        <li><a href="#">Table Tennis</a></li>
+        <li><a href="#">Rugby</a></li> 
+    </ul>
+
+   
+    <section class="main-img">
+        <img src="images/slider1.webp" alt="" width="100%">
+        <div class="img-box1">
+            <img src="images/banner-sport1.webp" alt="">
+            <img src="images/sportbook.webp" alt="">
+            <div>
+            <img class="aviator" src="images/aviator-banner (1).webp" alt="">
+            <h2>Aviator</h2>
+            </div>
+             <div>
+             <img src="images/mac88-730_280.webp" alt="">
+             <h2>MCA88</h2>
+             </div>
+            
+        </div>
+        <img src="images/fungames_1920x500.webp" width="100%" alt="">
+        <H2>FUN GAMES</H2>
+        <div class="img-box2">
+            <img src="images/aesexy.webp" alt="">
+            <img src="images/monopoly.webp" alt="">
+            <img src="images/dreamcatcher.webp" alt="">
+            <img src="images/dragontiger.webp" alt="">
+            <div>
+            <img src="images/blackjack.webp" alt="">
+            <h2>Black Jack</h2>
+            </div>
+              <div>
+              <img src="images/coin-toss.webp" alt="">
+              <h2>Coin Toss</h2>
+              </div>
+              <div>
+              <img src="images/baccarat.webp" alt="">
+              <h2>Baccarat</h2>
+              </div>
+
+                 <div>
+                 <img src="images/balloon.webp" alt="">
+                 <h2>Balloon</h2>
+                 </div>
+            
+            <img src="images/cricketx.webp" alt="">
+            <img src="images/footballx.webp" alt="">
+            <img src="images/gmx_wonhundred.webp" alt="">
+            <img src="images/inc5.webp" alt="">
+        </div>
+        <div class="img-box1">
+            <img src="images/intcasino.webp" alt="">
+            <img src="images/livecasino.webp" alt="">
+        </div>
+        <div class="img-box2">
+            <div>
+            <img src="images/mines.webp" alt="">
+            <h2>Mines</h2>
+            </div>
+           <div>
+           <img src="images/pilot.webp" alt="">
+           <h2>Pilot</h2>
+           </div>
+          <div>
+          <img src="images/roulette.webp" alt="">
+          <h2>Roulette</h2>
+          </div>
+           <div>
+           <img src="images/rummy.webp" alt="">
+           <h2>Rummy</h2>
+           </div>
+            
+            <img src="images/sicbo.webp" alt="">
+            <img src="images/sms-jetex.webp" alt="">
+            <img src="images/spinx.webp" alt="">
+            <img src="images/vikings.webp" alt="">
+        </div>
+        <div class="img-box1">
+            <div>
+            <img src="images/greyhoundracing.webp" alt="">
+            <h2>Greyhound Racing</h2>
+            </div>
+           <div>
+           <img src="images/horseracing.webp" alt="">
+           <h2>Horse Racing</h2>
+           </div>
+           
+        </div>
+    </section>
+
+    <footer class="footer">
+        <div class="footer1">
+            <div class="img1">
+                <img src="Images/laser1.png" alt="">
+            </div>
+            <img src="Images/laser2.png" alt="">
+            <div>
+                <p>Laser247 is the trading name of Sports Target B.V., a company incorporated and regulated in Curaçao
+                    under company number 148053 with it's registered office at Fransche Bloemweg 4, Willemstad, Curaçao.
+                </p>
+            </div>
+        </div>
+        <div class="footer2">
+            <p class="footerP">Our website works best in the newest and last prior version of these browsers: Google
+                Chrome.
+                Firefox</p>
+
+            <div class="com">
+                <a href="">LASERWIN247@GMAIL.COM</a>
+            </div>
+
+            <ul>
+                <li> <a href="">Privacy Policy</a></li>
+                <li> <a href="">Terms And Conditions</a></li>
+                <li> <a href="">Rules And Regulations</a></li>
+                <li> <a href="">About Us</a></li>
+                <li> <a href="">Responsible Gaming</a></li>
+            </ul>
+            <img class="imgLast" src="Images/laser3.png" alt="">
+        </div>
+
+    </footer>
+</body>
+
+</html>
